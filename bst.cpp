bad75520fef0c1c60f28e83d75c14c102570ce03
Arijit kar <arijitkar29@gmail.com>
2022-08-28 17:10:13 +0530

    k

---
+++ bst.cpp
@@ -0,0 +1,51 @@
+#include "bits/stdc++.h"
+#define inf 99999999
+using namespace std;
+
+
+class node{  
+    public:
+    int data;
+    node* left;
+    node*right;
+    node(int val){
+        data=val;
+        left=NULL;
+        right=NULL;
+    }
+};
+
+
+void preorder(struct node *temp) {
+  if (temp != NULL) {
+    cout << " " << temp->data;
+    preorder(temp->left);
+    preorder(temp->right);
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(){
+
+node * root =new node(7);
+// cout<<"it is root value:"<<root->data<<"\n";
+  root->left = new node(2);
+  root->right = new node(3);
+  root->left->left = new node(4);
+preorder(root);
+  cout<<"\n";
+
+    return 0;
+}
--- double-linked_list.cpp
+++ double-linked_list.cpp
@@ -5,6 +5,9 @@
 #include<stdlib.h>
 using namespace std;
   
+
+
+
 class node{
     public:
     int data;
@@ -18,11 +21,63 @@ class node{
 
     }
 };
-void insert(node *&tail, int val, int pos){
+
+int length(node *head);
+
+void insert(node *&head, int val, int pos){
+    node* temp=new node(val);
+    node* itr=head;
+    int siz=length(head);
+
+    if (head ==NULL){
+        head=temp;
+    }
+    else if  (pos=siz+1){
+        while(itr->next){
+            itr=itr->next;
+
+        }
+        itr->next=temp;
+        temp->prev=itr;
+
+    }
+    else if (pos==1){
+        temp->next=head;
+        head->prev=temp;
+        head=temp;
+
+    }
+    else{
+        while(itr){cout<<5;}
+    }
 
     
 }
+int length(node *head)
+{
+    node *ptr = head;
+    // ptr = head;
+    int count = 0;
+    while (ptr)
+    {
+        // cout << ptr->data << " ";
+        count++;
+        ptr = ptr->next;
+    }
+    return count;
+}
 
+void display(node *head)
+{
+    node *ptr = head;
+    // ptr = head;
+    while (ptr != NULL)
+    {
+        cout << ptr->data << " ";
+        ptr = ptr->next;
+    }
+    cout << endl;
+}
 
  
  
@@ -30,7 +85,15 @@ void insert(node *&tail, int val, int pos){
 int main()
 {
  node* head=new node(89);
- 
+    int siz=length(head);
+ insert(head,49,length(head)+1);
+ insert(head,98,length(head)+1);
+ insert(head,78,length(head)+1);
+ insert(head,45,length(head)+1);
+ insert(head,99,length(head)+1);
+ insert(head,29,length(head)+1);
+
+display(head);
  
  
     return 0;
