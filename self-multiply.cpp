2f84ee46971f884f9af153ff307a3239b2f85941
Arijit kar <arijitkar29@gmail.com>
2022-09-04 17:50:43 +0530

    y

---
+++ self-multiply.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    // nums: given vector
+    // return the Product vector P that hold product except self at each index
+    vector<long long int> productExceptSelf(vector<long long int>& nums, int n) {
+        long long prod=1;
+       for (int i=0;i<n;++i){
+           prod=prod*nums[i];
+       }
+    //   vector </
+       vector<long long int> ans;
+       for (int i=0;i<n;++i){
+           long long  b=prod/nums[i];
+        //    ans[i]=b;
+        ans.push_back(b);
+       }
+       return ans;
+        //code here        
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+ {
+    int t;  // number of test cases
+    cin>>t;
+    while(t--)
+    {
+        int n;  // size of the array
+        cin>>n;
+        vector<long long int> arr(n),vec(n);
+        
+        for(int i=0;i<n;i++)    // input the array
+        {
+            cin>>arr[i];
+        }
+        Solution obj;
+        vec = obj.productExceptSelf(arr,n);   // function call
+        
+        for(int i=0;i<n;i++)    // print the output
+        {
+            cout << vec[i] << " ";
+        }
+        cout<<endl;
+    }
+	return 0;
+}
+// } Driver Code Ends
