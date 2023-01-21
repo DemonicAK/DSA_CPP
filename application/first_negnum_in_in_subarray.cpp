a4c06dddb24d4babc191564a8147dbb07c400eed
Arijit kar <arijitkar29@gmail.com>
2023-01-21 11:14:39 +0530

    practice

---
+++ application/first_negnum_in_in_subarray.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<long long> printFirstNegativeInteger(long long int A[],
+                                            long long int N, long long int K)
+{
+
+    queue<int> neg;
+    for (int y = 0; y < K; y++)
+    {
+        if (A[y] < 0)
+            neg.push(A[y]);
+    }
+    vector<long long> ans;
+    int i = 0, j = i + K-1;
+    while (j < N)
+    {
+
+        if (A[j] < 0)
+            neg.push(A[j]);
+
+        if (neg.empty())
+            ans.push_back(0);
+        else
+            ans.push_back(neg.front());
+
+        if (neg.front() == A[i])
+            neg.pop();
+
+        i++;
+        j++;
+    }
+    return ans;
+}
+int main()
+{
+
+
+    long long int N, K;
+    cin >> N >> K;
+    long long int A[N];
+    for (long long int i = 0; i < N; i++)
+        cin >> A[i];
+    vector<long long> ans = printFirstNegativeInteger(A, N, K);
+    for (long long int i = 0; i < ans.size(); i++)
+        cout << ans[i] << " ";
+
+    return 0;
+}
--- application/stocks_buy_and_sell.cpp
+++ application/stocks_buy_and_sell.cpp
@@ -1,98 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+// Function to find the days of buying and selling stock for max profit.
 vector<vector<int>> stockBuySell(vector<int> A, int n)
 {
     // code here
-// int buy_ind=0,sell_ind=0;
-//     int profit_tillnow = 0;
-//     int buy = INT_MAX;
-
-//     vector<vector<int>> ans;
-
-//     for (int i = 0; i < n; i++)
-//     {
-//     int sell =A[i];
-//       int  profit = sell - buy;
-//         if (profit<0)
-//         {
-//             buy_ind = i;
-//             buy = A[i];
-//         }
-//         else if ( profit_tillnow = profit)
-//         {
-//             profit_tillnow=profit;
-//             sell_ind = i;
-//             ans.push_back({buy_ind,sell_ind});
-//         }
-//         else if ( profit_tillnow < profit)
-//         {
-//             profit_tillnow=profit;
-//             sell_ind = i;
-//             ans.pop_back();
-//             ans.push_back({buy_ind,sell_ind,profit_tillnow});
-//         }
-//     }
-//     vector<vector<int>> ansi;
-
-// int max_profit=0;
-
-// for(int i=0;i<ans.size();i++){
-// int curr_profit=ans[i][2];
-// if(curr_profit>max_profit){
-// max_profit=curr_profit;
-
-
-
-// }
-
-
-    // return ans;
-
-    vector<vector<int>> res;
-
-    int lastBuy = 0;
-
-    for (int i = 1; i < n; i++)
-
+    bool stock = false;
+    int buyd = 0;
+    int selld = 0;
+    vector<vector<int>> ans;
+    for (int i = 0; i < n; i++)
     {
+        int today = A[i];
+        int tomorow = A[i + 1];
+        if (!stock)
+        {
+            // to buy
+            if (today < tomorow)
+            {
+                stock = true;
+                buyd = i;
+            }
+        }
+        else
+        {
+            // to sell
+            if (today > tomorow)
+            {
+                stock = false;
+                selld = i;
 
-if (A[i] > A[lastBuy])
-
-{
-
-    res.push_back({lastBuy, i});
-}
-
-lastBuy = i;
+                vector<int> A{buyd, selld};
+                ans.push_back(A);
+            }
+        }
     }
-
-return res;
-
-
-
-
+    return ans;
 }
 
-
-
-
-
-
-int main()
-{
+int main(){
 
     // N = 7
-    vector<int> A = {100, 180, 260, 310, 40, 535, 695};
+    vector<int> A = {4, 2, 2, 2, 4};
 
+vector<vector<int>> ans = stockBuySell(A, A.size());
 
-
-    vector<vector<int>> ans = stockBuySell(A, A.size());
-
-
-    for (int i = 0; i < ans.size(); i++)
-    {
-        cout << ans[i][0] << " " << ans[i][1] << endl;
+for (int i = 0; i < ans.size(); i++)
+{
+    cout << ans[i][0] << " " << ans[i][1] << endl;
     }
 
     return 0;
